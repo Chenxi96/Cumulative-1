5c5bfa73482cd354a4f2209df55e50a8ad989aca
Chenxi Lin <lin.chenxi14@gmail.com>
2025-07-27 16:28:18 -0400

    Add debug files

--- Cumulative1/obj/Debug/net8.0/Cumulative1.AssemblyInfo.cs
+++ Cumulative1/obj/Debug/net8.0/Cumulative1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cumulative1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83f333a4ecde68560677d2d75ca8e8cb13e8d44f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c1c9bb328fb2ccd476e76c648a53b01d7e812b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cumulative1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cumulative1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
